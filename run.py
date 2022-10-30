05786c539be02bf18847b0149188effa397fb2e9
jomari <jomarialang31@gmail.com>
2022-10-30 14:18:40 +0000

    fix(hosting): chagned debug to false

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
-
+import os
 
 app = create_app() 
 
@@ -8,4 +8,6 @@ app = create_app()
 
 if __name__ == '__main__': 
 
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT') or 5000),
+            debug=False)
